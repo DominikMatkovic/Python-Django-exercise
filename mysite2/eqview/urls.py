0ac410bb16c620b57c202865b89be52cc3a88fbc
DominikMatkovic <domba.matko@gmail.com>
2021-10-29 16:18:26 +0200

    Added list of people, list of equipment, adding people, adding equipment, now working on deleting data

--- mysite2/eqview/urls.py
+++ mysite2/eqview/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.detail, name='detail'),
+    path('people/<int:id>/', views.peopleDetail, name='peopleDetail'),
+    path('addPersonForm/', views.addPersonForm, name='addPersonForm'),
+    path('addEquipmentForm/', views.addEquipmentForm, name='addEquipmentForm'),
 ]
--- mysite2/eqview/views.py
+++ mysite2/eqview/views.py
@@ -1,11 +1,55 @@
+from typing import ContextManager
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Equipment
 from .models import Person
 
 
 def index(request):
-    return HttpResponse("<b>Hello</b> <h1>...</h1>")
+    eq_list  = Equipment.objects.all()
+    people_list  = Person.objects.all()
+    context = {'eq_list': eq_list, 'people_list': people_list }
+    return render(request, 'eqview/index.html', context)
 
 
 def detail(request, id):
-    return HttpResponse("You're looking at person %s." % id)
+    EquipmentById = Equipment.objects.get(pk = id)
+    context = {'EquipmentById': EquipmentById}
+    return render(request, 'eqview/eqDetails.html', context)
+
+def peopleDetail(request, id):
+    PersonById = Person.objects.get(pk = id)
+    context = {'PersonById': PersonById}
+    return render(request, 'eqview/peopleDetails.html', context)
+
+
+def addPersonForm(request):
+    if request.method == 'POST':
+        if request.POST.get('OIB'):
+            person=Person()
+            person.OIB= request.POST.get('OIB')
+            person.save()
+
+
+    return render(request,'eqview/addPersonForm.html') 
+
+
+def addEquipmentForm(request):
+    people_list  = Person.objects.all()
+    context = {'people_list': people_list }
+
+    if request.method == 'POST':
+        if request.POST.get('serialNum') and request.POST.get('OIB'):
+            equipment=Equipment()
+            equipment.serialNum= request.POST.get('serialNum')
+            #equipment.borrowedBy= request.POST.get('OIB')
+
+            if request.POST.get('OIB') != "None":
+                equipment.borrowedBy = Person.objects.get(OIB = request.POST.get('OIB'))
+
+
+
+            equipment.save()
+
+
+    return render(request,'eqview/addEquipmentForm.html',context) 
