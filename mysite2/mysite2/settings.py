eeae8e074411418e97631509d00cbcc41cf893c3
DominikMatkovic <domba.matko@gmail.com>
2021-11-03 13:56:03 +0100

    Started with bootstrap

--- mysite2/mysite2/settings.py
+++ mysite2/mysite2/settings.py
@@ -32,12 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'eqview.apps.EqviewConfig',
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
